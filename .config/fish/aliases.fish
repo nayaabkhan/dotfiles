152461a440e795488abda30f63caea02e64c11a1
Nayaabkhan Khan <khannayaab@gmail.com>
2019-02-09 18:11:11 +0530

    Remove unknown aliases

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -11,8 +11,3 @@ function dl       ; cd ~/Downloads ; end
 function ip       ; curl -s http://checkip.dyndns.com/ | sed 's/[^0-9\.]//g' ; end
 function localip  ; ipconfig getifaddr en0 ; end
 function lookbusy ; cat /dev/urandom | hexdump -C | grep --color "ca fe" ; end
-
-# mv, rm, cp
-alias mv 'command gmv --interactive --verbose'
-alias rm 'command grm --interactive --verbose'
-alias cp 'command gcp --interactive --verbose'
