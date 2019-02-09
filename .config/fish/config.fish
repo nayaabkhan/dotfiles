f8ebe98a9b0d05d4f7a08acc273590ae6ad60939
Nayaabkhan Khan <khannayaab@gmail.com>
2019-02-09 18:08:15 +0530

    Set GPG_TTY for signing commits

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 set -U fish_greeting ""
 
+set -x GPG_TTY (tty)
 set -gx GO_PATH ~/Projects/go
 set -g fish_user_paths "/usr/local/opt/node@8/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_paths
