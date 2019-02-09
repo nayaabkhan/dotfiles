f97dffe1bc0ef2b0930b6bba702602265fe27383
Nayaabkhan Khan <khannayaab@gmail.com>
2019-02-09 17:16:05 +0530

    Add fish config

---
+++ .config/fish/aliases.fish
@@ -0,0 +1,18 @@
+# Navigation
+function ..    ; cd .. ; end
+function ...   ; cd ../.. ; end
+function ....  ; cd ../../.. ; end
+function ..... ; cd ../../../.. ; end
+
+# Utilities
+function g        ; git $argv ; end
+function grep     ; command grep --color=auto $argv ; end
+function dl       ; cd ~/Downloads ; end
+function ip       ; curl -s http://checkip.dyndns.com/ | sed 's/[^0-9\.]//g' ; end
+function localip  ; ipconfig getifaddr en0 ; end
+function lookbusy ; cat /dev/urandom | hexdump -C | grep --color "ca fe" ; end
+
+# mv, rm, cp
+alias mv 'command gmv --interactive --verbose'
+alias rm 'command grm --interactive --verbose'
+alias cp 'command gcp --interactive --verbose'
