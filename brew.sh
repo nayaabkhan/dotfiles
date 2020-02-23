bd921b505b50194f257e85f4982a6a26a0d779bc
Nayaabkhan Khan <khannayaab@gmail.com>
2020-02-23 18:07:32 +0530

    Update brew list

--- brew.sh
+++ brew.sh
@@ -39,41 +39,73 @@ fi;
 brew install gnupg
 
 # Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
-brew install grep
-brew install openssh
-brew install git
-brew install hub
-brew install htop
-brew install gotop
-brew install bat
 brew install asciinema
+brew install awscli
+brew install bat
+brew install cmus
+brew install ffmpeg
+brew install fish
+brew install flac
+brew install gh
+brew install git
+brew install gnupg
+brew install go
+brew install gotop
+brew install grep
+brew install htop
+brew install hub
+brew install jq
+brew install kubectx
+brew install kubernetes-cli
+brew install mas
+brew install minikube
+brew install neofetch
 brew install node@12
+brew install openssl
+brew install screenfetch
+brew install the_silver_searcher
 brew install tldr
-brew install tmux
 brew install trash
 brew install yarn
 brew install youtube-dl
-brew install mas
-brew install the_silver_searcher
-brew install vifm
-brew install clang-format
-brew install cmake
-brew install cmus
-brew install kubectx
 
 # Install macOS apps.
+brew cask install airtable
 brew cask install alfred
+brew cask install android-platform-tools
 brew cask install appcleaner
+brew cask install balenaetcher
+brew cask install brave-browser
 brew cask install caffeine
 brew cask install docker
 brew cask install dropbox
+brew cask install figma
+brew cask install firefox
 brew cask install font-hasklig
+brew cask install font-hasklig-nerd-font
+brew cask install font-jetbrains-mono
+brew cask install google-chrome
+brew cask install graphiql
+brew cask install graphql-playground
+brew cask install hyper
+brew cask install imageoptim
 brew cask install iterm2
+brew cask install karabiner-elements
 brew cask install keka
 brew cask install keycastr
-brew cask install night-owl
-brew cask install pocket-casts
+brew cask install macdown
+brew cask install minikube
+brew cask install mos
+brew cask install nightowl
+brew cask install notion
+brew cask install numi
+brew cask install openemu
+brew cask install plug
+brew cask install pock
+brew cask install postman
+brew cask install qlimagesize
+brew cask install sequel-pro
+brew cask install sip
 brew cask install skype
 brew cask install slack
 brew cask install spectacle
@@ -83,15 +115,10 @@ brew cask install teamviewer
 brew cask install tor-browser
 brew cask install transmission
 brew cask install tunnelbear
+brew cask install virtualbox
 brew cask install visual-studio-code
 brew cask install vlc
 brew cask install vox
-brew cask install plug
-brew cask install sloth
-brew cask install ubersicht
-brew cask install balenaetcher
-brew cask install icons8
-brew cask install pock
 
 
 # Remove outdated versions from the cellar.
