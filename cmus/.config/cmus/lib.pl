433192519602846adada881db6bfff625149a24d
Nayaabkhan Khan <nayaab@hey.com>
2021-09-03 18:46:00 +0530

    Update cmus playlist

--- cmus/.config/cmus/lib.pl
+++ cmus/.config/cmus/lib.pl
@@ -1,3 +1,5 @@
+/home/nayaabkhan/Dropbox/Music/__/00.mp3
+/home/nayaabkhan/Dropbox/Music/_/[138]/00.mp3
 /home/nayaabkhan/Dropbox/Music/Adnan Sami/1920/06 Vaada Tumse Hain Vaada.mp3
 /home/nayaabkhan/Dropbox/Music/Amaal Mallik & Rochak Kohli/M.S. Dhoni - The Untold Story/02 Kaun Tujhe.flac
 /home/nayaabkhan/Dropbox/Music/Amit Trivedi/Kai Po Che/01 Manja.mp3
@@ -5,6 +7,15 @@
 /home/nayaabkhan/Dropbox/Music/Amit Trivedi/Kai Po Che/03 Meethi Boliyaan.mp3
 /home/nayaabkhan/Dropbox/Music/Ankit Tiwari & Amaal Mallik/Airlift/05 Soch Na Sake.mp3
 /home/nayaabkhan/Dropbox/Music/Ankit Tiwari, Mithoon & Rabbi/Ek Villain/02 Banjaara.mp3
+/home/nayaabkhan/Dropbox/Music/Anu Malik/Phir Teri Kahani Yaad Aayi/01 Tere Dar Par Sanam Hum.flac
+/home/nayaabkhan/Dropbox/Music/Anu Malik/Murder/02 Bheegey Hont.mp3
+/home/nayaabkhan/Dropbox/Music/Anu Malik/Murder/03 Kaho Na Kaho.mp3
+/home/nayaabkhan/Dropbox/Music/Anu Malik/Murder/04 Kaho Na Kaho (Remix).mp3
+/home/nayaabkhan/Dropbox/Music/Anu Malik/Murder/05 Dil Ko Hazar Bar.mp3
+/home/nayaabkhan/Dropbox/Music/Anu Malik/Murder/06 Zindagi Is Tarah.mp3
+/home/nayaabkhan/Dropbox/Music/Anu Malik/Murder/07 Zindagi Is Tarah.mp3
+/home/nayaabkhan/Dropbox/Music/Anu Malik/Murder/08 Mainu Tere Naal (Punjabi Soul Mix).mp3
+/home/nayaabkhan/Dropbox/Music/Anu Malik/Dum Laga Ke Haisha/01 Moh Moh Ke Dhaage (Male).flac
 /home/nayaabkhan/Dropbox/Music/Anu Malik, Mithoon & Roop Kumar Rathod/Zeher/01 Agar Tum Mil Jao (Female).flac
 /home/nayaabkhan/Dropbox/Music/Anu Malik, Mithoon & Roop Kumar Rathod/Zeher/02 Woh Lamhe Woh Baatein.flac
 /home/nayaabkhan/Dropbox/Music/Anu Malik, Mithoon & Roop Kumar Rathod/Zeher/03 Jaane Ja Jaane Ja.flac
@@ -21,25 +32,81 @@
 /home/nayaabkhan/Dropbox/Music/A. R. Rahman/Jab Tak Hai Jaan/04 Heer.mp3
 /home/nayaabkhan/Dropbox/Music/A. R. Rahman/Jab Tak Hai Jaan/05 Jiya Re.mp3
 /home/nayaabkhan/Dropbox/Music/Blue Swede/Hooked On A Feeling/01 Hooked on a Feeling.flac
+/home/nayaabkhan/Dropbox/Music/Camila Cabello feat. Young Thug/Havana/01 Havana.flac
+/home/nayaabkhan/Dropbox/Music/Charlie Puth/Nine Track Mind/05 We Don’t Talk Anymore.flac
+/home/nayaabkhan/Dropbox/Music/Charlie Puth/Attention/01 Attention.flac
 /home/nayaabkhan/Dropbox/Music/Clean Bandit feat. Sean Paul & Anne‐Marie/Rockabye/01 Rockabye (Robby East & STVCKS remix).flac
+/home/nayaabkhan/Dropbox/Music/Daniela Andrade/La Vie En Rose/01 La Vie En Rose.flac
+/home/nayaabkhan/Dropbox/Music/David Guetta/One More Love/03 Memories.flac
 /home/nayaabkhan/Dropbox/Music/DJ Snake/Encore/13 Let Me Love You.flac
+/home/nayaabkhan/Dropbox/Music/Eagles/Hotel California/01 Hotel California.flac
 /home/nayaabkhan/Dropbox/Music/Ed Sheeran/÷ (Divide)/04 Shape of You.flac
 /home/nayaabkhan/Dropbox/Music/Ed Sheeran/÷ (Divide)/06 Galway Girl.flac
 /home/nayaabkhan/Dropbox/Music/Eminem/The Eminem Show/18 ’Till I Collapse.flac
 /home/nayaabkhan/Dropbox/Music/Eminem/Curtain Call_ The Hits/06 Lose Yourself.flac
 /home/nayaabkhan/Dropbox/Music/Eurythmics/Greatest Hits_ Eurythmics/01 Sweet Dreams (Are Made of This).mp3
 /home/nayaabkhan/Dropbox/Music/Felix Jaehn feat. Jasmine Thompson/Ain’t Nobody (Loves Me Better)/01 Ain’t Nobody (Loves Me Better).mp3
+/home/nayaabkhan/Dropbox/Music/Florence + the Machine/Jenny of Oldstones (Game of Thrones)/01 Jenny of Oldstones (Game of Thrones).flac
+/home/nayaabkhan/Dropbox/Music/Grissini Project/Grissini Project _ Musique de jeux vidéos, animés et films/03 Lilium (From ''Elfen Lied'').flac
+/home/nayaabkhan/Dropbox/Music/Haddaway/The Album/01 What Is Love.flac
 /home/nayaabkhan/Dropbox/Music/Himesh Reshammiya/Namastey London/01 Chakna Chakna.mp3
 /home/nayaabkhan/Dropbox/Music/Himesh Reshammiya/Namastey London/04 Yahi Hota Pyaar.mp3
-/home/nayaabkhan/Dropbox/Music/Jeet Gannguli/Citylights/02 Muskurane (unplugged).mp3
+/home/nayaabkhan/Dropbox/Music/Howard Shore/Spotlight_ Original Motion Picture Soundtrack/01 Spotlight.mp3
+/home/nayaabkhan/Dropbox/Music/Howard Shore/Spotlight_ Original Motion Picture Soundtrack/02 Deference and Complicity.mp3
+/home/nayaabkhan/Dropbox/Music/Howard Shore/Spotlight_ Original Motion Picture Soundtrack/03 Investigative Journalism.mp3
+/home/nayaabkhan/Dropbox/Music/Howard Shore/Spotlight_ Original Motion Picture Soundtrack/04 Legacy.mp3
+/home/nayaabkhan/Dropbox/Music/Howard Shore/Spotlight_ Original Motion Picture Soundtrack/05 The Directories.mp3
+/home/nayaabkhan/Dropbox/Music/Howard Shore/Spotlight_ Original Motion Picture Soundtrack/06 Keep Silent.mp3
+/home/nayaabkhan/Dropbox/Music/Howard Shore/Spotlight_ Original Motion Picture Soundtrack/07 Summer Investigation.mp3
+/home/nayaabkhan/Dropbox/Music/Howard Shore/Spotlight_ Original Motion Picture Soundtrack/08 The Children.mp3
+/home/nayaabkhan/Dropbox/Music/Howard Shore/Spotlight_ Original Motion Picture Soundtrack/09 Pressure of the Church.mp3
+/home/nayaabkhan/Dropbox/Music/Howard Shore/Spotlight_ Original Motion Picture Soundtrack/10 The Sealed Documents.mp3
+/home/nayaabkhan/Dropbox/Music/Howard Shore/Spotlight_ Original Motion Picture Soundtrack/11 The Globe Newsroom.mp3
+/home/nayaabkhan/Dropbox/Music/Howard Shore/Spotlight_ Original Motion Picture Soundtrack/12 Courthouse.mp3
+/home/nayaabkhan/Dropbox/Music/Howard Shore/Spotlight_ Original Motion Picture Soundtrack/13 Practice and Policy.mp3
+/home/nayaabkhan/Dropbox/Music/Howard Shore/Spotlight_ Original Motion Picture Soundtrack/14 City on the Hill.mp3
+/home/nayaabkhan/Dropbox/Music/Howard Shore/Spotlight_ Original Motion Picture Soundtrack/15 Pain and Anguish.mp3
+/home/nayaabkhan/Dropbox/Music/Howard Shore/Spotlight_ Original Motion Picture Soundtrack/16 Night Mass.mp3
+/home/nayaabkhan/Dropbox/Music/Howard Shore/Spotlight_ Original Motion Picture Soundtrack/17 Delivering the News.mp3
+/home/nayaabkhan/Dropbox/Music/Howard Shore/Spotlight_ Original Motion Picture Soundtrack/18 The Story Breaks.mp3
+/home/nayaabkhan/Dropbox/Music/Jagjit Singh/Arth/05 Koi Yeh Kaise Bataye.flac
+/home/nayaabkhan/Dropbox/Music/Jeet Gannguli/Citylights/02 Muskurane (unplugged).flac
 /home/nayaabkhan/Dropbox/Music/Jeet Gannguli, Mithoon & Ami Mishra/Hamari Adhuri Kahani/02 Humnava.mp3
 /home/nayaabkhan/Dropbox/Music/Jeet Gannguli, Mithoon & Ami Mishra/Hamari Adhuri Kahani/03 Hasi (Male Version).mp3
 /home/nayaabkhan/Dropbox/Music/Jeet Gannguli & Sangeet-Siddharth/Raaz Reboot_ Original Motion Picture Soundtrack/01 Lo Maan Liya.mp3
 /home/nayaabkhan/Dropbox/Music/Jeet Gannguli & Sangeet-Siddharth/Raaz Reboot_ Original Motion Picture Soundtrack/02 Raaz Aankhein Teri.mp3
 /home/nayaabkhan/Dropbox/Music/Jeet Gannguli & Sangeet-Siddharth/Raaz Reboot_ Original Motion Picture Soundtrack/04 Yaad Hai Na.mp3
 /home/nayaabkhan/Dropbox/Music/Jeet Gannguli & Sangeet-Siddharth/Raaz Reboot_ Original Motion Picture Soundtrack/05 Hummein Tummein Jo Tha.mp3
-/home/nayaabkhan/Dropbox/Music/Jonas Blue featuring JP Cooper/Perfect Strangers/01 Perfect Strangers.flac
+/home/nayaabkhan/Dropbox/Music/Junoon/Azadi/01 Sayonee.flac
+/home/nayaabkhan/Dropbox/Music/Justin Hurwitz, Benj Pasek & Justin Paul/La La Land_ Original Motion Picture Soundtrack/01 Another Day of Sun.flac
+/home/nayaabkhan/Dropbox/Music/Justin Hurwitz, Benj Pasek & Justin Paul/La La Land_ Original Motion Picture Soundtrack/02 Someone in the Crowd.flac
+/home/nayaabkhan/Dropbox/Music/Justin Hurwitz, Benj Pasek & Justin Paul/La La Land_ Original Motion Picture Soundtrack/03 Mia & Sebastian’s Theme.flac
+/home/nayaabkhan/Dropbox/Music/Justin Hurwitz, Benj Pasek & Justin Paul/La La Land_ Original Motion Picture Soundtrack/04 A Lovely Night.flac
+/home/nayaabkhan/Dropbox/Music/Justin Hurwitz, Benj Pasek & Justin Paul/La La Land_ Original Motion Picture Soundtrack/06 City of Stars.flac
+/home/nayaabkhan/Dropbox/Music/Justin Hurwitz, Benj Pasek & Justin Paul/La La Land_ Original Motion Picture Soundtrack/07 Planetarium.flac
+/home/nayaabkhan/Dropbox/Music/Justin Hurwitz, Benj Pasek & Justin Paul/La La Land_ Original Motion Picture Soundtrack/08 Summer Montage _ Madeline.flac
+/home/nayaabkhan/Dropbox/Music/Justin Hurwitz, Benj Pasek & Justin Paul/La La Land_ Original Motion Picture Soundtrack/09 City of Stars.flac
+/home/nayaabkhan/Dropbox/Music/Justin Hurwitz, Benj Pasek & Justin Paul/La La Land_ Original Motion Picture Soundtrack/11 Engagement Party.flac
+/home/nayaabkhan/Dropbox/Music/Justin Hurwitz, Benj Pasek & Justin Paul/La La Land_ Original Motion Picture Soundtrack/12 Audition (The Fools Who Dream).flac
+/home/nayaabkhan/Dropbox/Music/Justin Hurwitz, Benj Pasek & Justin Paul/La La Land_ Original Motion Picture Soundtrack/13 Epilogue.flac
+/home/nayaabkhan/Dropbox/Music/Justin Hurwitz, Benj Pasek & Justin Paul/La La Land_ Original Motion Picture Soundtrack/14 The End.flac
+/home/nayaabkhan/Dropbox/Music/Justin Hurwitz, Benj Pasek & Justin Paul/La La Land_ Original Motion Picture Soundtrack/15 City of Stars (Humming).flac
+/home/nayaabkhan/Dropbox/Music/Justin Timberlake/CAN’T STOP THE FEELING!/01 CAN’T STOP THE FEELING! (original song from DreamWorks Animation’s “Trolls”).flac
+/home/nayaabkhan/Dropbox/Music/Kiiara/Low Kii Savage/01 Gold.flac
+/home/nayaabkhan/Dropbox/Music/Kygo/Stargazing/02 It Ain't Me.flac
+/home/nayaabkhan/Dropbox/Music/Lana Del Rey/Venice Bitch/01 Venice Bitch.flac
+/home/nayaabkhan/Dropbox/Music/Led Zeppelin/Led Zeppelin III/01 Immigrant Song.flac
+/home/nayaabkhan/Dropbox/Music/Led Zeppelin/[Led Zeppelin IV]/04 Stairway to Heaven.flac
+/home/nayaabkhan/Dropbox/Music/Linked Horizon/Jiyuu e no Shingeki/01 Guren no Yumiya.m4a
+/home/nayaabkhan/Dropbox/Music/Lucky Ali/Sunoh/01 O Sanam.flac
+/home/nayaabkhan/Dropbox/Music/Lucky Ali/Sifar/02 Dekha Hai Aise Bhi.flac
 /home/nayaabkhan/Dropbox/Music/Luis Fonsi feat. Daddy Yankee/Despacito/01 Despacito.flac
+/home/nayaabkhan/Dropbox/Music/Madan Mohan/Veer-Zaara/01 Tere Liye.flac
+/home/nayaabkhan/Dropbox/Music/Madan Mohan/Veer-Zaara/02 Main Yahaan Hoon.flac
+/home/nayaabkhan/Dropbox/Music/Madan Mohan/Veer-Zaara/04 Yeh Hum Aa Gaye Hain Kahaan.flac
+/home/nayaabkhan/Dropbox/Music/Madan Mohan/Veer-Zaara/05 Do Pal.flac
+/home/nayaabkhan/Dropbox/Music/Madan Mohan/Veer-Zaara/06 Kyon Hawa.flac
+/home/nayaabkhan/Dropbox/Music/Madan Mohan/Veer-Zaara/08 Aaya Tere Dar Par.flac
 /home/nayaabkhan/Dropbox/Music/Major Lazer feat. Justin Bieber & MØ/Cold Water/01 Cold Water.flac
 /home/nayaabkhan/Dropbox/Music/Major Lazer x DJ Snake feat. MØ/Lean On/01 Lean On.flac
 /home/nayaabkhan/Dropbox/Music/Mike Posner/I Took a Pill in Ibiza (SeeB Remix)/01 I Took a Pill in Ibiza (SeeB remix).flac
@@ -49,31 +116,65 @@
 /home/nayaabkhan/Dropbox/Music/Nadeem-Shravan/Raaz/01 Jo Bhi Kasmein.mp3
 /home/nayaabkhan/Dropbox/Music/Nadeem-Shravan/Raaz/07 Pyaar Se Pyaar Hum.mp3
 /home/nayaabkhan/Dropbox/Music/Nadeem-Shravan/Raaz/08 Ye Shaher Hai Aman Ka (Shanti Shanti).mp3
+/home/nayaabkhan/Dropbox/Music/Nadeem-Shravan/Bewafaa/10 Teri... Yaad... Yaad... Yaad.flac
+/home/nayaabkhan/Dropbox/Music/Nancy Sinatra & Lee Hazlewood/Nancy & Lee/04 Summer Wine.flac
+/home/nayaabkhan/Dropbox/Music/New Arcades/Carry On/01 Carry On.flac
 /home/nayaabkhan/Dropbox/Music/Nusrat Fateh Ali Khan & Rahat Fateh Ali Khan/Mere rashke qamar/01 Mere rashke qamar.mp3
+/home/nayaabkhan/Dropbox/Music/Paradise Lunch/Gun’s & Roses/01 Gun’s & Roses.m4a
+/home/nayaabkhan/Dropbox/Music/Pixie Lott/Turn It Up/07 My Love.mp3
+/home/nayaabkhan/Dropbox/Music/Plain White T’s/All That We Needed/13 Hey There Delilah.mp3
+/home/nayaabkhan/Dropbox/Music/Plain White T’s/Big Bad World/05 1, 2, 3, 4.mp3
 /home/nayaabkhan/Dropbox/Music/Pritam/Jab We Met/06 Aaoge Jab Tum.mp3
+/home/nayaabkhan/Dropbox/Music/Pritam/Singh is Kinng/04 Teri Ore.flac
 /home/nayaabkhan/Dropbox/Music/Pritam/Ae Dil Hai Mushkil/01 Ae Dil Hai Mushkil.flac
 /home/nayaabkhan/Dropbox/Music/Pritam/Ae Dil Hai Mushkil/02 Bulleya.flac
 /home/nayaabkhan/Dropbox/Music/Pritam/Ae Dil Hai Mushkil/03 Channa Mereya.flac
 /home/nayaabkhan/Dropbox/Music/Qurat-ul-Ain Balouch/Humsafar/01 Woh Humsafar Tha.mp3
+/home/nayaabkhan/Dropbox/Music/Rahat Fateh Ali Khan & Momina Mustehsan/Afreen Afreen (Coke Studio Season 9)/01 Afreen Afreen.flac
+/home/nayaabkhan/Dropbox/Music/Rajesh Roshan/Kaho Naa... Pyaar Hai/03 Na Tum Jano Na Hum.flac
 /home/nayaabkhan/Dropbox/Music/Rajesh Roshan/Kites/01 Zindagi Do Pal Ki.mp3
 /home/nayaabkhan/Dropbox/Music/Rajesh Roshan/Kites/02 Dil Kyun Yeh Mera.mp3
 /home/nayaabkhan/Dropbox/Music/Raju Singh/Raaz - The Mystery Continues.._/01 Maahi.mp3
+/home/nayaabkhan/Dropbox/Music/Ramin Djawadi/Game of Thrones_ Music From the HBO Series, Season 6/03 Light of the Seven.flac
+/home/nayaabkhan/Dropbox/Music/Ramin Djawadi/Westworld_ Season 1 (Selections from the HBO® Series)/01 Main Title Theme - Westworld.flac
 /home/nayaabkhan/Dropbox/Music/Ram Sampath/Satyamev Jayate/01 Satyamev Jayate.mp3
+/home/nayaabkhan/Dropbox/Music/Robin Schulz/Sugar/01 Sugar.mp3
+/home/nayaabkhan/Dropbox/Music/Sachin-Jigar/Shuddh Desi Romance/02 Gulabi.mp3
 /home/nayaabkhan/Dropbox/Music/Sajid-Wajid/Dabangg/01 Tere Mast Mast Do Nain.mp3
 /home/nayaabkhan/Dropbox/Music/Sajid-Wajid/Dabangg/03 Chori Kiya Re Jiya.mp3
 /home/nayaabkhan/Dropbox/Music/Salim-Sulaiman/Rab Ne Bana Di Jodi/01 Tujh Mein Rab Dikhta Hain.mp3
-/home/nayaabkhan/Dropbox/Music/Sohail Sen/Mere Brother Ki Dulhan/03 Choomantar.mp3
-/home/nayaabkhan/Dropbox/Music/Sohail Sen/Mere Brother Ki Dulhan/04 Isq Risk.mp3
+/home/nayaabkhan/Dropbox/Music/Selena Gomez/Back to You/01 Back to You.mp3
+/home/nayaabkhan/Dropbox/Music/Shankar–Ehsaan–Loy/Bhaag Milkha Bhaag/06 Slow Motion Angreza.mp3
+/home/nayaabkhan/Dropbox/Music/Shankar–Ehsaan–Loy & Gulzar/Bunty Aur Babli/06 Kajra Re.mp3
+/home/nayaabkhan/Dropbox/Music/Strings/Duur/02 Duur.flac
+/home/nayaabkhan/Dropbox/Music/Sufjan Stevens/Carrie & Lowell/01 Death With Dignity.flac
+/home/nayaabkhan/Dropbox/Music/Sufjan Stevens/Carrie & Lowell/02 Should Have Known Better.flac
+/home/nayaabkhan/Dropbox/Music/Sufjan Stevens/Carrie & Lowell/07 The Only Thing.flac
+/home/nayaabkhan/Dropbox/Music/Sufjan Stevens/Mystery of Love/01 Mystery of Love.flac
+/home/nayaabkhan/Dropbox/Music/The Beatles/The Beatles/07 While My Guitar Gently Weeps.flac
+/home/nayaabkhan/Dropbox/Music/The Chainsmokers/Collage/03 Closer.flac
+/home/nayaabkhan/Dropbox/Music/The Chainsmokers/Memories…Do Not Open/05 Something Just Like This.mp3
+/home/nayaabkhan/Dropbox/Music/The Chainsmokers & Coldplay/Something Just Like This/01 Something Just Like This.flac
+/home/nayaabkhan/Dropbox/Music/The Chainsmokers ft. Daya/Don’t Let Me Down/01 Don’t Let Me Down.flac
+/home/nayaabkhan/Dropbox/Music/The Chainsmokers ft. Halsey/Closer/01 Closer.mp3
 /home/nayaabkhan/Dropbox/Music/The Chainsmokers ft. XYLØ/Setting Fires/01 Setting Fires.flac
+/home/nayaabkhan/Dropbox/Music/The Handsome Family/Singing Bones/05 Far From Any Road.mp3
+/home/nayaabkhan/Dropbox/Music/The Midnight/Days of Thunder/02 Gloria.flac
+/home/nayaabkhan/Dropbox/Music/The Midnight/Days of Thunder/06 Los Angeles.flac
 /home/nayaabkhan/Dropbox/Music/The Midnight/Sunset/01 Sunset.mp3
+/home/nayaabkhan/Dropbox/Music/The Midnight/Days Of Thunder (Case Conflict)/02 Days Of Thunder.flac
+/home/nayaabkhan/Dropbox/Music/The Midnight/Endless Summer/04 Jason.flac
+/home/nayaabkhan/Dropbox/Music/The Midnight/Endless Summer/08 Vampires.flac
+/home/nayaabkhan/Dropbox/Music/The Midnight/Endless Summer/12 Memories.flac
+/home/nayaabkhan/Dropbox/Music/The Midnight/Monsters/12 Deep Blue.flac
 /home/nayaabkhan/Dropbox/Music/The Police/Synchronicity/07 Every Breath You Take.mp3
+/home/nayaabkhan/Dropbox/Music/The Weeknd ft. Daft Punk/Starboy/01 Starboy.flac
 /home/nayaabkhan/Dropbox/Music/The Weeknd ft. Daft Punk/I Feel It Coming/01 I Feel It Coming.flac
-/home/nayaabkhan/Dropbox/Music/Usher/Yeah!/01 Yeah!.mp3
-/home/nayaabkhan/Dropbox/Music/Usher/More/02 More.mp3
-/home/nayaabkhan/Dropbox/Music/Usher featuring Pitbull/DJ Got Us Fallin’ in Love/01 DJ Got Us Fallin’ in Love.mp3
-/home/nayaabkhan/Dropbox/Music/Usher feat. will.i.am/OMG/01 OMG.mp3
+/home/nayaabkhan/Dropbox/Music/Tinie Tempah feat. Zara Larsson/Girls Like/01 Girls Like.mp3
 /home/nayaabkhan/Dropbox/Music/Compilations/Raajneeti/01 Bheegi Si Bhaagi Si.mp3
 /home/nayaabkhan/Dropbox/Music/Compilations/Raajneeti/02 Mora Piya.mp3
+/home/nayaabkhan/Dropbox/Music/Compilations/Coke Studio Season 8/04 Tajdar-E-Haram.flac
+/home/nayaabkhan/Dropbox/Music/Compilations/Coke Studio, Season 9/16 Paar Chanaa De.flac
 /home/nayaabkhan/Dropbox/Music/Compilations/13 Reasons Why_ A Netflix Original Series Soundtrack/01 Only You.flac
 /home/nayaabkhan/Dropbox/Music/Compilations/13 Reasons Why_ A Netflix Original Series Soundtrack/02 Kill Em With Kindness (acoustic).flac
 /home/nayaabkhan/Dropbox/Music/Compilations/13 Reasons Why_ A Netflix Original Series Soundtrack/03 Bored.flac
@@ -87,7 +188,16 @@
 /home/nayaabkhan/Dropbox/Music/Compilations/13 Reasons Why_ A Netflix Original Series Soundtrack/11 Fascination Street (remastered).flac
 /home/nayaabkhan/Dropbox/Music/Compilations/13 Reasons Why_ A Netflix Original Series Soundtrack/12 The Walls Came Down (single version).flac
 /home/nayaabkhan/Dropbox/Music/Compilations/13 Reasons Why_ A Netflix Original Series Soundtrack/13 The Stand (long version).flac
+/home/nayaabkhan/Dropbox/Music/Compilations/Everything, Everything_ Original Motion Picture Soundtrack/07 Stay.flac
+/home/nayaabkhan/Dropbox/Music/Compilations/Coke Studio Season 10_ Episode 2/02 Sayonee.flac
+/home/nayaabkhan/Dropbox/Music/Compilations/Coke Studio Season 10_ Episode 4/02 Dekh Tera Kya & Latthay Di Chaadar.flac
+/home/nayaabkhan/Dropbox/Music/Compilations/Coke Studio Season 10_ Epsiode 6/03 Kaatay Na Katay.flac
+/home/nayaabkhan/Dropbox/Music/Compilations/Coke Studio Season 10_ Episode 7/02 Baanware.flac
+/home/nayaabkhan/Dropbox/Music/Compilations/Coke Studio Season 10_ Episode 7/04 Us Rah Par.flac
+/home/nayaabkhan/Dropbox/Music/Compilations/Call Me By Your Name_ Original Motion Picture Soundtrack/17 Visions of Gideon.flac
+/home/nayaabkhan/Dropbox/Music/Compilations/Coke Studio Season 11_ Sound of the Nation/16 Malang.flac
 /home/nayaabkhan/Dropbox/Music/Compilations/Pure Synthpop, Vol. 1/03 Summer Lights.flac
+/home/nayaabkhan/Dropbox/Music/Compilations/Marjaavaan/01 Tum Hi Aana.mp3
 /home/nayaabkhan/Dropbox/Music/Vishal Bhardwaj/Maachis/08 Chappa Chappa.mp3
 /home/nayaabkhan/Dropbox/Music/Vishal Bhardwaj/Ek Thi Daayan/01 Yaaram.mp3
 /home/nayaabkhan/Dropbox/Music/Vishal-Shekhar/Shanghai/01 Bharat Mata Ki Jai.mp3
@@ -95,3 +205,7 @@
 /home/nayaabkhan/Dropbox/Music/Vishal-Shekhar/Shanghai/04 Khudaaya.mp3
 /home/nayaabkhan/Dropbox/Music/Vishal-Shekhar/Shanghai/05 Morcha.mp3
 /home/nayaabkhan/Dropbox/Music/Vishal-Shekhar/Befikre/01 Nashe Si Chadh Gayi.mp3
+/home/nayaabkhan/Dropbox/Music/タニウチヒデキ & 平野義久/Death Note Original Soundtrack/03 Raito No Tēma.mp3
+/home/nayaabkhan/Dropbox/Music/タニウチヒデキ & 平野義久/Death Note Original Soundtrack/04 Eru No Tēma.mp3
+/home/nayaabkhan/Dropbox/Music/島みやえい子/Higurashi no Naku Koro ni/01 Higurashi no Naku Koro ni.mp3
+/home/nayaabkhan/Dropbox/Music/島みやえい子/E.P.S -Eiko PRIMARY SELECTION-/01 ひぐらしのなく頃に.flac
