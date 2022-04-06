129c18e3eb7398a256bf809e0448a6ed843e8401
Nayaabkhan Khan <nayaab@hey.com>
2022-04-06 09:34:44 +0530

    bat: Use OneHalfDark theme

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,5 @@
 # enable vim mode
 fish_vi_key_bindings
 
-source ~/.config/fish/aliases.fish
 source ~/.config/fish/env.fish
-
-# pure prompt customisation
-set pure_threshold_command_duration 2
+source ~/.config/fish/aliases.fish
