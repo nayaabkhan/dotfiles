2f73f2b3f47ba87cc11595bf372adf2ec6b72c58
Nayaabkhan Khan <nayaab@hey.com>
2023-02-17 11:59:47 +0530

    fish: Show different cursor in different vim modes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,9 @@
 # enable vim mode
 fish_vi_key_bindings
+set fish_cursor_default     block      blink
+set fish_cursor_insert      line       blink
+set fish_cursor_replace_one underscore blink
+set fish_cursor_visual      block
 
 source ~/.config/fish/env.fish
 source ~/.config/fish/aliases.fish
